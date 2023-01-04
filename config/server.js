cbb0a27832a877b56f1ab510c2b6f645c56ace5a
tom.beachell@enorm.com <Forza12!a>
2023-01-04 16:57:13 +0100

    returining

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', 'strapi-production-f621.up.railway.app'),
+  host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array("APP_KEYS"),
